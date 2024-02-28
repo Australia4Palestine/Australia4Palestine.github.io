a51a4d6293c346d45e405478088d24ee735afe76
leontoys <svliyon@gmail.com>
2024-02-28 21:06:46 +1100

    combined into one repo

--- README.md
+++ README.md
@@ -1,31 +1,5 @@
-# Australia4Palestine.github.io
-
-Welcome to the Australia4Palestine GitHub repository! This repository hosts the static website for Australia's support for Palestine.
-
-## About the Website
-
-This website serves as a platform to demonstrate Australia's solidarity and support for Palestine. It aims to raise awareness about the ongoing issues faced by the Palestinian people and advocate for justice, peace, and human rights in the region.
-
-## How to Contribute
-
-Contributions to the Australia4Palestine website are welcome! If you have suggestions for content, improvements to the design, or any other ideas to enhance the website, please feel free to contribute. Here's how you can do it:
-
-1. **Fork the Repository:** Start by forking this repository to your GitHub account.
-
-2. **Make Changes:** Make the desired changes to the code, content, or design of the website.
-
-3. **Submit a Pull Request:** Once you're done with your changes, submit a pull request to merge your changes into the main branch of this repository.
-
-4. **Review and Collaborate:** Your pull request will be reviewed by the repository maintainers. Feel free to engage in discussions and collaborate with others to refine your contributions.
-
-## License
-
-This project is licensed under the [MIT License](LICENSE). By contributing to this repository, you agree to abide by the terms of this license.
-
-## Contact
-
-If you have any questions, suggestions, or concerns regarding the Australia4Palestine website or this repository, please don't hesitate to contact us at [your-email@example.com].
-
-Thank you for your interest and support!
-
 [![Ceasefire Now](https://badge.techforpalestine.org/default)](https://techforpalestine.org/learn-more)
+
+This project is licensed under the [GNU General Public License v3.0](LICENSE). Your contributions are welcome!
+
+Visit the [Australia4Palestine](https://australia4palestine.github.io/) website to learn more.
---
+++ scripts/mps.js
@@ -0,0 +1,84 @@
+// Initialize the map
+var map = L.map('map').setView([-25.2744, 133.7751], 5);
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Custom control for the map title
+var titleControl = L.control({ position: 'topleft' });
+
+titleControl.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'map-title');
+    div.innerHTML = '<h1 style="color: blue; text-align: center; margin: 0;">MPs for Palestine</h1>';
+    return div;
+};
+
+titleControl.addTo(map);
+
+//Mobile
+map.locate({ setView: true, maxZoom: 5 });
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are here ").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    L.marker([-33.865143, 151.209900]).addTo(map)
+        .bindPopup("Default Location ").openPopup();
+}
+
+map.on('locationerror', onLocationError);
+
+// Define a custom marker icon with a watermelon image
+var watermelonIcon = L.icon({
+    iconUrl: 'images/watermelon-icon.png', // URL to the watermelon image file
+    iconSize: [40, 40], // Size of the icon
+    iconAnchor: [20, 40], // Point of the icon which corresponds to marker's location
+    popupAnchor: [0, -40] // Point from which the popup should open relative to the iconAnchor
+});
+
+// Define a custom marker icon with a watermelon image
+var pinIcon = L.icon({
+    iconUrl: 'images/pin.png', // URL to the watermelon image file
+    iconSize: [20, 20], // Size of the icon
+    iconAnchor: [20, 40], // Point of the icon which corresponds to marker's location
+    popupAnchor: [0, -40] // Point from which the popup should open relative to the iconAnchor
+});
+
+// Parse CSV data and add markers to the map
+Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vR1_69lZekJC45D2YJUOfLNwf0To7PMKm6aPNqqZkkkV4_wbtSqOChVDR9ZLe0yLZRWy1qd8mZ5f9HR/pub?output=csv", {
+    download: true,
+    header: true,
+    complete: function (results) {
+        results.data.forEach(function (row) {
+            // Extract latitude and longitude from the row
+            var latitude = parseFloat(row.Latitude);
+            var longitude = parseFloat(row.Longitude);
+            if (row.Voted == 'Yes') {
+                // Create a marker with a popup showing MP's information
+                L.marker([latitude, longitude], { icon: watermelonIcon })
+                    .bindPopup('<b>' + row['Honorific'] + ' ' + row['Surname'] + ', ' + row['First Name'] + '</b><br>' +
+                        'Electorate: ' + row['Electorate'] + '<br>' +
+                        'Political Party: ' + row['Political Party'])
+                    .addTo(map);
+            } else {
+                // Create a marker with a popup showing MP's information
+                L.marker([latitude, longitude], { icon: pinIcon })
+                    .bindPopup('<b>' + row['Honorific'] + ' ' + row['Surname'] + ', ' + row['First Name'] + '</b><br>' +
+                        'Electorate: ' + row['Electorate'] + '<br>' +
+                        'Political Party: ' + row['Political Party'])
+                    .addTo(map);
+            }
+        });
+    }
+});
